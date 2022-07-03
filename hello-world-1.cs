94c1e42418de8cf98ee18720f2a6f23cea5c1d58
Kavindu Nimsara <kavibro000@gmail.com>
2022-07-03 15:40:40 +0530

    Create hello-world-1.cs

---
+++ hello-world-1.cs
@@ -0,0 +1,13 @@
+// Namespace Declaration
+using System;
+
+// Program start class
+class HelloWorld
+{
+    // Main begins program execution.
+    static void Main()
+    {
+        // Write to console
+      Console.WriteLine("Hello World"); 
+    }
+}
